9033b273c5ffb4d1536cf843929b914cdd12497e
Conor <t00193238@365s.ittralee.ie>
2017-11-28 09:33:06 +0000

    AddMember upgrades
    Adding addstandard functions from previous method to this way

--- src/addMember.java
+++ src/addMember.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class addMember {
     JTextField poundsField;
@@ -10,7 +12,7 @@ public class addMember {
     public addMember()
     {
         JFrame main = new JFrame("Add Member ");
-        main.setSize(200,200);
+        main.setSize(600,600);
 
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -18,40 +20,87 @@ public class addMember {
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         String[] experiencelvls = { "Novice", "Recreational", "Competitive"};
 
-        JTextField fname = new JTextField(10);
-        JTextField sname = new JTextField(10);
-        JTextField gender = new JTextField(10);
-        JTextField email = new JTextField(10);
-        JTextField phone = new JTextField(10);
-        JTextField age = new JTextField(10);
-        JTextField height = new JTextField(10);
-        JComboBox experience = new JComboBox(experiencelvls);
+        JTextField fnameBox = new JTextField(10);
+        JTextField snameBox = new JTextField(10);
+        JTextField genderBox = new JTextField(10);
+        JTextField emailBox = new JTextField(10);
+        JTextField phoneBox = new JTextField(10);
+        JTextField ageBox = new JTextField(10);
+        JTextField heightBox = new JTextField(10);
+        JComboBox experienceBox = new JComboBox(experiencelvls);
+        JCheckBox paidBox = new JCheckBox();
 
        // main.setLayout(new FlowLayout());
         main.setLayout(new GridLayout(0,1));
-        JPanel pane = new JPanel(new GridLayout(9,1));
+        JPanel pane = new JPanel(new GridLayout(10,1));
         main.add(pane);
         pane.add(new JLabel("Forename: "));
-        pane.add(fname);
+        pane.add(fnameBox);
         pane.add(new JLabel("Surname: "));
-        pane.add(sname);
+        pane.add(snameBox);
         pane.add(new JLabel("Gender: "));
-        pane.add(gender);
+        pane.add(genderBox);
         pane.add(new JLabel("Email: "));
-        pane.add(email);
+        pane.add(emailBox);
         pane.add(new JLabel("Phone: "));
-        pane.add(phone);
+        pane.add(phoneBox);
         pane.add(new JLabel("Age: "));
-        pane.add(age);
+        pane.add(ageBox);
         pane.add(new JLabel("Height: "));
-        pane.add(height);
+        pane.add(heightBox);
         pane.add(new JLabel("Experience: "));
-        pane.add(experience);
-
+        pane.add(experienceBox);
+        pane.add(new JLabel("Paid Fee: "));
+        pane.add(paidBox);
         JButton submit = new JButton("Save");
         pane.add(submit);
+
+    try {
+        String fname = fnameBox.getText();
+        String sname = snameBox.getText();
+        String gender = genderBox.getText();
+        String email = emailBox.getText();
+        String phone = phoneBox.getText();
+        String ageAsString = ageBox.getText();
+        int age = Integer.parseInt(ageAsString);
+        String heightAsString = phoneBox.getText();
+        int height = Integer.parseInt(heightAsString);
+        String experience = experienceBox.getName();
+
+
+
+
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/uuuu");
+        String dateregistered = date.format(formatter);
+
+
+
+        String coachname;
+        int coachnum;
+        String status = "Active";
+
+        if (age < 18) {
+            coachname = "Mark O'Donnell";
+            coachnum = 1;
+        } else if (age > 18 && age <= 35) {
+            coachname = "Peter Andrews";
+            coachnum = 2;
+        } else {
+            coachname = "David Edgar";
+            coachnum = 3;
+        }
+    }
+    catch (NumberFormatException n) {
+        System.out.print("Number format exception");
+    }
+      //  Standard standard = new Standard(fname, sname, gender, email, phone, age, height, dateregistered, paid, status, coachname, coachnum);
+
         main.pack();
         main.setVisible(true);
+
+
+
     }
 
 
